7bf2eb85ad564da39fc72340cb303f5244a1ad2f
EllinFeh <elisonfelipe16@hotmail.com>
2023-10-03 20:30:30 -0300

    signup

--- src/components/registerPage/styled.js
+++ src/components/registerPage/styled.js
@@ -3,12 +3,22 @@ import styled from "styled-components";
 
 export const Flex = styled.div`
 display: flex;
+
+border: 1px solid white;
 `
 
 export const Column = styled.div`
 display: flex;
 flex-direction: column;
 text-align: center;
+`
+
+export const Astrounaut = styled.div`
+
+.AstronautImage{
+    width: 290px;
+    height: 350px;
+}
 `
 
 
